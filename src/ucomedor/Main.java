4f530455518cfc2ce99cc4178840e8da12e2e0ef
Carlos Sánchez <casaar97@gmail.com>
2019-12-03 19:55:25 +0100

    Proyecto final

---
+++ src/ucomedor/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucomedor;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main {
+    
+    public static void main(String args[]) 
+    {
+        InicioSesion ventana = new InicioSesion();
+        ventana.setVisible(true);
+    }
+    
+}
